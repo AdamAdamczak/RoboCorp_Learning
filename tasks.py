62c46e04bd067847030613f4bdb4ba8bb627af0b
aadamczak <adamadamczak123@gmail.com>
2024-02-13 19:57:41 +0100

    Login and Fill form

--- tasks.py
+++ tasks.py
@@ -11,6 +11,22 @@ def open_the_internet():
         slowmo=100,
     )
     browser.goto("https://robotsparebinindustries.com/")
+    page = login()
+    fill_form(page)
 
+def login():
+    """Login to the internet."""
+    page = browser.page()
+    page.fill('#username', 'maria')
+    page.fill('#password', 'thoushallnotpass')
+    page.click("button:text('Log in')")
+    return page
+def fill_form(page):
+    """Fill out a form."""
+    page.fill('#firstname', 'Maria')
+    page.fill('#lastname', 'Smith')
+    page.select_option('#salestarget','65000')
+    page.fill('#salesresult', '123')
     
-
+    page.click("button:text('Submit')")
+    return page
